22d36faaeb30ab5d18924ff81b876e23a6821410
JF Denise <jdenise@redhat.com>
2020-11-17 17:55:04 +0100

    Cleanup endpoint for better close

--- tests/src/test/resources/projects/jaxrs/src/main/java/org/wildfly/plugins/demo/jaxrs/HelloWorldEndpoint.java
+++ tests/src/test/resources/projects/jaxrs/src/main/java/org/wildfly/plugins/demo/jaxrs/HelloWorldEndpoint.java
@@ -15,16 +15,20 @@ public class HelloWorldEndpoint {
     @GET
     @Produces("text/plain")
     public Response doGet() throws IOException {
-        InputStream inputStream = HelloWorldEndpoint.class.getResourceAsStream("/myresources.properties");
-        Properties props = new Properties();
-        props.load(inputStream);
-        inputStream.close();
+        Properties props;
+        try (InputStream inputStream = HelloWorldEndpoint.class.getResourceAsStream("/myresources.properties")) {
+            props = new Properties();
+            props.load(inputStream);
+        }
         InputStream inputStream2 = HelloWorldEndpoint.class.getResourceAsStream("/myresources2.properties");
         Properties props2 = null;
         if (inputStream2 != null) {
-            props2 = new Properties();
-            props2.load(inputStream2);
-            inputStream2.close();
+            try {
+                props2 = new Properties();
+                props2.load(inputStream2);
+            } finally {
+                inputStream2.close();
+            }
         }
 
         return Response.ok("Hello from " + props.getProperty("msg") + (props2 == null ? "" : " " + props2.getProperty("msg"))).build();
